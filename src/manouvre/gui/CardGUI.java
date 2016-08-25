bc1bc26b0bdae0537bcf3df2bf3fd6780620c049
Bartosz <Bartosz@CHRONOS>
2016-08-25 13:38:49 +0200

    Merge origin/master
    Conflicts:
    	nbproject/project.properties

--- src/manouvre/gui/CardGUI.java
+++ src/manouvre/gui/CardGUI.java
@@ -5,10 +5,46 @@
  */
 package manouvre.gui;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import manouvre.game.Card;
+import manouvre.game.Unit;
+//import java.awt.image.BufferedImage;
 /**
  *
  * @author Bartosz
  */
 public class CardGUI {
+    
+    Image imgFull;
+    
+    public CardGUI(int ID) {
+        Card card = new Card(ID);
+        try {
+            imgFull = ImageIO.read(new File("resources\\units\\"+card.getCardName()));
+        } catch (IOException ex) {
+            Logger.getLogger(UnitGUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    public CardGUI(Card card) {
+        try {
+            imgFull = ImageIO.read(new File("resources\\units\\"+  card.getCardName()   ));
+        } catch (IOException ex) {
+            Logger.getLogger(UnitGUI.class.getName()).log(Level.SEVERE, null, ex);
+        }                   
+    }   
+    
+    public Image getImgFull() {
+        return imgFull;
+    }
+    
+
+     
     
 }
