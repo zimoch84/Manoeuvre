ce0ce49e64a56a4c86aeb20775675f29b0b59bdc
Piotr <Piotr@Matrix.lan>
2017-04-03 21:56:41 +0200

    DrawLine function

--- src/manouvre/gui/GameGUI.java
+++ src/manouvre/gui/GameGUI.java
@@ -365,6 +365,37 @@ public class GameGUI {
             }
     }
     
+    /**
+      * Draw an arrow line betwwen two point 
+      * @param g the graphic component
+      * @param x1 x-position of first point
+      * @param y1 y-position of first point
+      * @param x2 x-position of second point
+      * @param y2 y-position of second point
+      * @param d  the width of the arrow
+      * @param h  the height of the arrow
+      */
+     private void drawArrowLine(Graphics g, int x1, int y1, int x2, int y2, int d, int h){
+        int dx = x2 - x1, dy = y2 - y1;
+        double D = Math.sqrt(dx*dx + dy*dy);
+        double xm = D - d, xn = xm, ym = h, yn = -h, x;
+        double sin = dy/D, cos = dx/D;
+
+        x = xm*cos - ym*sin + x1;
+        ym = xm*sin + ym*cos + y1;
+        xm = x;
+
+        x = xn*cos - yn*sin + x1;
+        yn = xn*sin + yn*cos + y1;
+        xn = x;
+
+        int[] xpoints = {x2, (int) xm, (int) xn};
+        int[] ypoints = {y2, (int) ym, (int) yn};
+
+        g.drawLine(x1, y1, x2, y2);
+        g.fillPolygon(xpoints, ypoints, 3);
+     }
+    
     private void drawRetrieving(Graphics g){
      
     if (mapGui.isUnitSelected()){
@@ -375,6 +406,7 @@ public class GameGUI {
                 
                 for (Position retrivingPositons: game.getRetreatPositions(selectedUnit))
                 {
+                    
                     g.drawLine(
                     (windowMode == CreateRoomWindow.AS_HOST) ? 
                             selectedUnit.getPosition().getMouseX() +  MapGUI.PIECE_WIDTH / 2 
